68f0a28a66611178a598ac1d9b3cb3a46a7cd170
GunjanSirohi <gunjansirohi4@gmail.com>
2025-08-19 11:09:38 +0530

    question

---
+++ ques12.java
@@ -0,0 +1,20 @@
+class que12{
+    
+    final int userId;
+    que12(int id){
+        this.userId=id;
+    }
+    void display(){
+        System.out.println(userId);
+    }
+}
+public class ques12 {
+
+    public static void main(String[] args) {
+        que12 u=new que12(101);
+        u.display();
+    }
+}
+
+
+
