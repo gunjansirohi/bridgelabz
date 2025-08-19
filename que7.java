5c00fb88ce0e6b73242dcac8e8d03c7c3f580e04
GunjanSirohi <gunjansirohi4@gmail.com>
2025-08-19 10:43:48 +0530

    question

---
+++ que7.java
@@ -0,0 +1,84 @@
+// public class ques1 {
+//     public static void main(String[] args) {
+//         Car c1=new Car("Honda Civic",2022);
+//         Car c2=new Car("Apache",2021);
+//         c1.display();
+//         c2.display();
+//     }
+// }
+// class Car{
+//     String model;
+//     int year;
+//     static int numberOfcar=0;
+//     Car(String model,int year){
+//         this.model=model;
+//         this.year=year;
+//         numberOfcar++;
+//     }
+//     void display(){
+//         System.out.println("model:"+model+"year: "+year);
+//         System.out.println("total cars:"+numberOfcar);
+//     }
+// }
+
+
+// public class ques3 {
+//     void display(){
+//         int a=10;
+//         System.out.println("a: "+a);
+//     }
+//     public static void main(String[] args) {
+//         ques3 s=new ques3();
+//         s.display();
+//     }
+// }
+//  question 5
+// class Mycar{
+//     static int numberofcar=0;
+//     Mycar(){
+//         numberofcar++;
+//     }
+     
+//     void change(int newcount){
+//         numberofcar=newcount;
+//     }
+//     void display(){
+//         System.out.println("total cars: "+numberofcar);
+//     }
+// }
+// public class ques5 {
+//     public static void main(String[] args) {
+//         Mycar c1=new Mycar();
+//         c1.change(10);
+//         c1.display();
+//     }
+// }
+
+
+// question 6
+//  class Demo{
+//     int number;  
+//     void setnumber(int number){
+        
+//         this.number=number;
+//     }
+//     void display(){
+//         System.out.println(number);
+//     }
+//  }
+// public class ques6 {
+//     public static void main(String[] args) {
+//         Demo d=new Demo();
+//         d.setnumber(50);
+//         d.display();
+//     }
+// }
+
+// question 7
+public class que7{
+     public static void main(String []args){
+      
+
+
+     }
+}
---
+++ que9.java
@@ -0,0 +1,19 @@
+
+    class Account {
+    String accountHolder;   
+    double balance;         
+    void display() {
+        System.out.println( accountHolder);
+        System.out.println( balance);
+    }
+}
+
+public class que9{
+    public static void main(String[] args) {
+    
+        Account acc = new Account();
+        acc.display();
+    }
+}
+
+
