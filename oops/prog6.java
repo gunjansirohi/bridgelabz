e24393b599780f87701296e9c3e436013ee775b3
GunjanSirohi <gunjansirohi4@gmail.com>
2025-09-22 11:24:36 +0530

    oops question

---
+++ oops/prog6.java
@@ -0,0 +1,17 @@
+package oops;
+
+public class prog6 {
+    static class Vehicle{
+        static int fee=100;
+        final double regno = 123456;
+        String ownername;
+        String vehicletype;
+        Vehicle(String ownername,String vehicletype)
+
+        void updateregfee(int regfee){
+            System.out.println("the reg fee is updated to " fee+regfee);
+        }
+    }
+    
+}
+
